55d9d7afd88ede645de2e69b7aec0237b194d278
EduardoCh93 <56979475+EduardoCh93@users.noreply.github.com>
2019-11-10 14:37:29 -0300

    Correcciones

--- src/main/java/fiuba/algo3/AlgoChess/acciones/Habilidad.java
+++ src/main/java/fiuba/algo3/AlgoChess/acciones/Habilidad.java
@@ -1,7 +1,6 @@
 package fiuba.algo3.AlgoChess.acciones;
 
 import fiuba.algo3.AlgoChess.entidades.Unidad;
-<<<<<<< Updated upstream
 import fiuba.algo3.AlgoChess.tableroycasilleros.Casillero;
 import fiuba.algo3.AlgoChess.tableroycasilleros.Tablero;
 
@@ -14,18 +13,9 @@ public abstract class Habilidad {
         this.rango = rango;
     }
 
-    protected ArrayList<Unidad> listaDeUnidadesAfectadas(int x, int y){
-        return rango.listaDeUnidadesAfectados(x , y);
+    protected ArrayList<Unidad> listaDeUnidadesAfectadas(int x, int y) {
+        return rango.listaDeUnidadesAfectados(x, y);
     }
 
-    public abstract void  activarHabilidad(Unidad unidad);
-
-
-public interface Habilidad {
-
-    public void  atacar();
-
-    public void curar(Unidad unidad);
-
-
+    public abstract void activarHabilidad(Unidad unidad);
 }
