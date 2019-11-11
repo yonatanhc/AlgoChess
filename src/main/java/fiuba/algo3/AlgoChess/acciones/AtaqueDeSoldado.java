0d0d0fbf4ca5e7fd937e6a65826d5fb604f75c21
yonatan <yohuca@icloud.com>
2019-11-11 15:30:38 -0300

    refactor ataque soldado

--- src/main/java/fiuba/algo3/AlgoChess/acciones/AtaqueDeSoldado.java
+++ src/main/java/fiuba/algo3/AlgoChess/acciones/AtaqueDeSoldado.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 public class AtaqueDeSoldado extends Habilidad {
     private int danioCuerpoACuerpo;
+    private Unidad unidadAtacante;
 
     public AtaqueDeSoldado(Tablero tablero){
         super(new Rango(tablero));
@@ -14,14 +15,33 @@ public class AtaqueDeSoldado extends Habilidad {
     }
 
     public void activarHabilidad(Unidad unidad){
+        this.unidadAtacante = unidad;
         ArrayList<Unidad> unidadesAfectados = rangoDelAtaque(unidad.getUbicacion().getX(),unidad.getUbicacion().getY());
-        //realizar ataque ala lista de unidades
+        /*
+        for (int i = 0; i < unidadesAfectados.size();i++){
+            unidadesAfectados.get(i).recibirDanio(this.danioCuerpoACuerpo);
+        }*/
+        if(unidadesAfectados.size() >= 0){
+            unidadesAfectados.get(0).recibirDanio(this.danioCuerpoACuerpo);
+        }
+
     }
 
     public ArrayList<Unidad> rangoDelAtaque(int x, int y){
         ArrayList<Unidad> unidadesAfectados = new ArrayList<Unidad>();
-        this.listaDeUnidadesAfectadas(x,y,1,unidadesAfectados);
-        this.listaDeUnidadesAfectadas(x,y,2,unidadesAfectados);
+        for(int i = 1; i <= 2;i++){
+            this.listaDeUnidadesAfectadas(x,y,i,unidadesAfectados);
+        }
+        filtrarUnidades(unidadesAfectados);
+
         return unidadesAfectados;
+    }
+
+    public void filtrarUnidades(ArrayList<Unidad> unidades){
+        for(int i = 0; i < unidades.size(); i++){
+            if(unidades.get(i).getJugador().equals(this.unidadAtacante.getJugador())){
+                unidades.remove(i);
+            }
+        }
     }
 }
