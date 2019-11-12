9333b9397ddab97c0e15b12244bdea917bbc00a5
Gonzalo Gabriel Rizzo <31524121+gonzalor95@users.noreply.github.com>
2019-11-11 23:14:37 -0300

    Se agregan pruebas del batallon

--- src/main/java/fiuba/algo3/AlgoChess/entidades/Batallon.java
+++ src/main/java/fiuba/algo3/AlgoChess/entidades/Batallon.java
@@ -3,6 +3,8 @@ package fiuba.algo3.AlgoChess.entidades;
 import fiuba.algo3.AlgoChess.acciones.Rango;
 import fiuba.algo3.AlgoChess.acciones.RangoContiguo;
 import fiuba.algo3.AlgoChess.tableroycasilleros.Casillero;
+import fiuba.algo3.AlgoChess.tableroycasilleros.CasilleroOcupadoException;
+import fiuba.algo3.AlgoChess.tableroycasilleros.ErrorDePosicionException;
 import fiuba.algo3.AlgoChess.tableroycasilleros.Tablero;
 
 import java.util.ArrayList;
@@ -26,25 +28,30 @@ public class Batallon {
         ArrayList<Unidad> unidadesCercanas = rango.listaDeUnidadesAfectados(liderBatallon.getUbicacion().getX(),liderBatallon.getUbicacion().getY());
 
         //Elimino si no es soldado y es el mismo soldado seleccionado
-        unidadesCercanas.removeIf(n -> n.getClass() != liderBatallon.getClass() && n == liderBatallon && n.getJugador() != liderBatallon.getJugador());
-       this.miembrosBatallon = unidadesCercanas;
-        // this.miembrosBatallon = unidadesCercanas.subList(1,2);
+        unidadesCercanas.removeIf(n -> n.getClass() != liderBatallon.getClass() || n == liderBatallon || n.getJugador() != liderBatallon.getJugador());
+        this.miembrosBatallon = unidadesCercanas;
+       //this.miembrosBatallon = unidadesCercanas.subList(1,3);
         this.miembrosBatallon.add(liderBatallon); //Me queda una lista con el soldado seleccionado y 2 contiguos
     }
 
-    public void moverBatallon(Casillero casilleroDestino){
+    public void moverBatallon(Casillero casilleroDestino) {
 
         int xOrigen = this.liderBatallon.getUbicacion().getX();
         int yOrigen = this.liderBatallon.getUbicacion().getY();
+        Casillero casilleroDestinoDeCadaUnidad;
 
         for(int i = 0; i<miembrosBatallon.size(); i++){
 
             int xdiff = xOrigen - this.miembrosBatallon.get(i).getUbicacion().getX();
             int ydiff = yOrigen - this.miembrosBatallon.get(i).getUbicacion().getY();
-            int xprueba = casilleroDestino.getX()-xdiff;
-            int yprueba = casilleroDestino.getY()-ydiff;
 
-            tablero.moverUnidadA(this.miembrosBatallon.get(i),tablero.obtenerCasillero(casilleroDestino.getX()-xdiff,casilleroDestino.getY()-ydiff));
+            //chequeamos si el casillero a moverse esta libre o si es válido. En otro caso, no hacemos nada
+            try {
+                casilleroDestinoDeCadaUnidad = tablero.obtenerCasillero(casilleroDestino.getX() - xdiff, casilleroDestino.getY() - ydiff);
+                tablero.moverUnidadA(this.miembrosBatallon.get(i), casilleroDestinoDeCadaUnidad);
+            }
+            catch (ErrorDePosicionException e){}
+            catch (CasilleroOcupadoException e){}
 
         }
 
--- src/test/java/fiuba/algo3/AlgoChess/TestUnidadMovible.java
+++ src/test/java/fiuba/algo3/AlgoChess/TestUnidadMovible.java
@@ -71,4 +71,69 @@ public class TestUnidadMovible {
 
     }
 
+    @Test
+    void test04BatallonUnoSeQuedaQuietoPorObstaculoDeCasilleroOcupado(){
+        Tablero tablero = new Tablero();
+        Soldado soldado1 = new Soldado();
+        Soldado soldado2 = new Soldado();
+        Soldado soldado3 = new Soldado();
+        Jinete jineteObstaculo = new Jinete();
+
+        Jugador jugador = new Jugador("jugador");
+        Batallon batallon;
+
+        Casillero ubicacion;
+
+        tablero.ingresarUnidadEn(soldado1,3,4,jugador);
+        tablero.ingresarUnidadEn(soldado2,2,3,jugador);
+        tablero.ingresarUnidadEn(soldado3,3,5,jugador);
+        tablero.ingresarUnidadEn(jineteObstaculo,2,4,jugador);
+
+        ubicacion = tablero.obtenerCasillero(2,4);
+
+        batallon = new Batallon(soldado1);
+
+        batallon.moverBatallon(ubicacion);
+
+        //verificamos que el soldado1, no se mueve de su lugar
+        assertEquals(tablero.obtenerCasillero(3, 4), soldado1.getUbicacion());
+        assertEquals(tablero.obtenerCasillero(1, 3), soldado2.getUbicacion());
+        assertEquals(tablero.obtenerCasillero(2, 5), soldado3.getUbicacion());
+
+        assertEquals(tablero.obtenerCasillero(2,4),jineteObstaculo.getUbicacion());
+
+    }
+
+    @Test
+    void test04BatallonUnoSeQuedaQuietoPorObstaculoDePosicionFueraDelTablero(){
+        Tablero tablero = new Tablero();
+        Soldado soldado1 = new Soldado();
+        Soldado soldado2 = new Soldado();
+        Soldado soldado3 = new Soldado();
+
+        Jugador jugador = new Jugador("jugador");
+        Batallon batallon;
+
+        Casillero ubicacion;
+
+        tablero.ingresarUnidadEn(soldado1,2,2,jugador);
+        tablero.ingresarUnidadEn(soldado2,1,1,jugador);
+        tablero.ingresarUnidadEn(soldado3,3,3,jugador);
+
+        ubicacion = tablero.obtenerCasillero(1,2);
+
+        batallon = new Batallon(soldado1);
+
+        batallon.moverBatallon(ubicacion);
+
+        //verificamos que el soldado1, no se mueve de su lugar
+        assertEquals(tablero.obtenerCasillero(1, 2), soldado1.getUbicacion());
+        assertEquals(tablero.obtenerCasillero(1, 1), soldado2.getUbicacion());
+        assertEquals(tablero.obtenerCasillero(2, 3), soldado3.getUbicacion());
+
+    }
+
+
+
+
 }
