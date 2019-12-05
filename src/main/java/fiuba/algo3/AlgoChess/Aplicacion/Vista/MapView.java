2b1c60e362dec4586be66d5f923bc3e02825a77b
Pabloale96 <pabloale96@gmail.com>
2019-12-05 12:14:47 -0300

    Se repara el bug de la muerte de la catapulta.

--- src/main/java/fiuba/algo3/AlgoChess/Aplicacion/Vista/MapView.java
+++ src/main/java/fiuba/algo3/AlgoChess/Aplicacion/Vista/MapView.java
@@ -302,8 +302,8 @@ public class MapView extends Group {
                 int y = unidad.getUbicacion().getY();
                 table.getChildren().remove(buttons[x][y]);
                 createButtonOnMap(x,y);
-                algoChess.obtenerTablero().removerUnidad(unidad.getUbicacion());
                 listPieceView.remove(i);
+                algoChess.obtenerTablero().removerUnidad(unidad.getUbicacion());
             }
         }
     }
